eb962095cf127ca8a6664f3635c9aa21473f629a
Ben Garraud <ben.garraud@jwt.com>
2013-06-21 12:29:56 +0100

    Fix a few bug from initial release

--- utils/timer.js
+++ utils/timer.js
@@ -8,39 +8,43 @@ define([], function () {
 		var _ref = this;
 		_ref.running = false;
 		_ref.once = function(time, callback) {
+			_ref.clear();
 			_time = time;
 			_callback = callback;
-			_ref.clear();
 			_tType = 1;
 	        _ref.running = true;
 	        _t = setTimeout(function(){
 	        		console.log('end timeout');
 	                if (typeof callback == 'function') callback();
-	                _callback=null;
 	                _ref.running = false;
 	        },time*1000);    
-
 		};
-
+		
 		_ref.every = function(time, callback) {
+			_ref.clear();
 			_time = time;
 			_callback = callback;
-			_ref.clear();
 			_tType = 0;
 	        _ref.running = true;
 	        _t = setInterval(function(){
-	            
-
-	                if (typeof callback == 'function') callback();
-	                _callback=null;
-	                _ref.running = false;
+	            if (typeof callback == 'function') callback();
+	            _callback=null;
+	            _ref.running = false;
 	        },time*1000);    
 
 		};
+		_ref.stop = function() {
+			if (_t!==null) {
+				if (_tType==1) {
+					clearTimeout(_t);
+				} else if (_tType==0) {
+					clearInterval(_t);
+				}
+			}
 
+		};
 		_ref.clear = function() {
 			if (_t!==null) {
-				
 				if (_tType==1) {
 					clearTimeout(_t);
 				} else if (_tType==0) {
@@ -50,19 +54,14 @@ define([], function () {
 				_ref.running = false;
 				_t = null;
 			}
-
 		};
-
-
 		_ref.reset = function() {
-			_ref.clear();
 			if (_time>0 && _callback) {
 				if (_tType==1) {
-					_ref.once(_t);
+					_ref.once(_time, _callback);
 				} else if (_time, _callback) {
-					_ref.every(t);
+					_ref.every(_time, _callback);
 				}
-
 			}
 		};
 	}	
